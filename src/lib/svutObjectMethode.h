2bcc44ebab1077117e791166890efece7def5c36
Valat Sébastien <sebastien.valat.dev@orange.fr>
2010-06-29 21:44:56 +0200

    * début d'importation des ancien codes

---
+++ src/lib/svutObjectMethode.h
@@ -0,0 +1,61 @@
+/*****************************************************
+             PROJECT  : svmath
+             VERSION  : 0.0.1
+             DATE     : 08/2009
+             AUTHOR   : Valat Sébastien
+             LICENSE  : CeCILL-C
+*****************************************************/
+
+#ifndef SVUT_OBJECT_METHODE_HEADER
+#define SVUT_OBJECT_METHODE_HEADER
+
+namespace svUnitTest
+{
+
+/********************  CLASSE  **********************/
+/**
+ * Classe de base utilisé pour créer une forme de pointeur de fonction pour les membres de classe.
+ * Cette classe de base doit être surchargée par une forme généric pour pouvoir prendre en charge
+ * tous les types de classes possible.
+ * @brief Classe de base définissant une forme de pointeur de fonction membre.
+ * @author Valat Sébastien
+ * @version 0.0.1
+**/
+class svutObjectMethode
+{
+	public:
+		svutObjectMethode() {};
+		virtual ~svutObjectMethode() {};
+		/**
+		 * Méthode permettant de lancer l'appel.
+		**/
+		virtual void call(void) = 0;
+};
+
+/********************  CLASSE  **********************/
+/**
+ * Implémentation générique  de svutObjectMethode afin de prendre en charge tous les types d'objets
+ * utilisés. Attention, l'implémentation de fonctionne pas pour les membres virtuels.
+ * @brief Implémentation générique de la classe pointeur de fonction.
+ * @author Valat Sébastien
+ * @version 0.0.1
+**/
+template <class T>
+class svutObjectMethodeGeneric : public svutObjectMethode
+{
+	public:
+		svutObjectMethodeGeneric(T * object,void (T::*methode)(void));
+		virtual void call(void);
+	protected:
+		/** Pointeur vers l'objet à utiliser pour appeler la méthode membre. **/
+		T * object;
+		/** Adresse de la méthode membre. **/
+		void (T::*methode)(void);
+};
+
+};
+
+/*****************  GENERIC IMPLE  ******************/
+#include "svutObjectMethodeGenericImpl.h"
+
+#endif
---
+++ src/lib/svutObjectMethodeGenericImpl.h
@@ -0,0 +1,42 @@
+/*****************************************************
+             PROJECT  : svmath
+             VERSION  : 0.0.1
+             DATE     : 08/2009
+             AUTHOR   : Valat Sébastien
+             LICENSE  : CeCILL-C
+*****************************************************/
+
+#ifndef SVUT_OBJECT_METHODE_GENERIC_IMPL_HEADER
+#define SVUT_OBJECT_METHODE_GENERIC_IMPL_HEADER
+
+
+/********************  HEADERS  *********************/
+#include "svutObjectMethode.h"
+
+namespace svUnitTest
+{
+
+/********************  METHODE  *********************/
+/**
+ * Constructeur de la classe.
+ * @param object Définit l'objet à utiliser pour appeler la méthode.
+ * @param methode Définit la méthode à appeler sur l'objet. Attention il ne peux pas s'agir d'une
+ * méthode virtuelle.
+**/
+template<class T>
+svutObjectMethodeGeneric<T>::svutObjectMethodeGeneric(T * object,void (T::*methode)(void))
+{
+	this->object = object;
+	this->methode = methode;
+}
+
+/********************  METHODE  *********************/
+template<class T>
+void svutObjectMethodeGeneric<T>::call(void)
+{
+	(object->*methode)();
+}
+
+};
+
+#endif
