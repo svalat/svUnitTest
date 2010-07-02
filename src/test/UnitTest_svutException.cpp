6a7ea1c3e31d516cc6ee5a7aa41631fd682f1146
Valat Sébastien <sebastien.valat.dev@orange.fr>
2010-07-02 21:16:37 +0200

    * test de svutException (ticket #12)

---
+++ src/test/UnitTest_svutException.cpp
@@ -0,0 +1,82 @@
+/*****************************************************
+             PROJECT  : svUnitTest
+             VERSION  : 0.0.2
+             DATE     : 06/2010
+             AUTHOR   : Valat Sébastien
+             LICENSE  : CeCILL-C
+*****************************************************/
+
+/********************  HEADERS  *********************/
+#include "UnitTestExtra.h"
+#include <iostream>
+#include <cppunit/TestCase.h>
+#include <cppunit/extensions/TestFactoryRegistry.h>
+#include <cppunit/extensions/HelperMacros.h>
+#include <svutException.h>
+
+using namespace svUnitTest;
+using namespace CPPUNIT_NS;
+
+/********************  CLASSE  **********************/
+class UnitTest_svutException : public TestCase
+{
+	CPPUNIT_TEST_SUITE(UnitTest_svutException);
+	CPPUNIT_TEST(testConstructor);
+	CPPUNIT_TEST(testConstructor_str);
+	CPPUNIT_TEST(testWhat);
+	CPPUNIT_TEST(testGetMessage);
+	CPPUNIT_TEST_SUITE_END();
+
+	public:
+		void setUp(void);
+		void tearDown(void);
+
+	protected:
+		void testConstructor(void);
+		void testConstructor_str(void);
+		void testWhat(void);
+		void testGetMessage(void);
+		
+		svutException * exc;
+};
+
+/********************  METHODE  *********************/
+void UnitTest_svutException::setUp(void)
+{
+	exc = new svutException("message");
+}
+
+/********************  METHODE  *********************/
+void UnitTest_svutException::tearDown(void)
+{
+	delete exc;
+}
+
+/********************  METHODE  *********************/
+void UnitTest_svutException::testConstructor(void)
+{
+	svutException obj;
+	CPPUNIT_ASSERT_EQUAL("",obj.getMessage());
+}
+
+/********************  METHODE  *********************/
+void UnitTest_svutException::testConstructor_str(void)
+{
+	svutException obj("message");
+	CPPUNIT_ASSERT_EQUAL("message",obj.getMessage());
+}
+
+/********************  METHODE  *********************/
+void UnitTest_svutException::testWhat(void)
+{
+	CPPUNIT_ASSERT(exc->what() != NULL);
+	CPPUNIT_ASSERT(strcmp("message",exc->what()) == 0);
+}
+
+/********************  METHODE  *********************/
+void UnitTest_svutException::testGetMessage(void)
+{
+	CPPUNIT_ASSERT_EQUAL("message",exc->getMessage());
+}
+
+CPPUNIT_TEST_SUITE_REGISTRATION(UnitTest_svutException);
