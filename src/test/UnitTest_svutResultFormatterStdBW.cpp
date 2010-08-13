87b6cae7fe739b6d239c5a1ab28ab4182f6307cb
Valat Sébastien <sebastien.valat.dev@orange.fr>
2010-08-13 19:43:20 +0200

    * finish ticket #49  * finish to implement tests of STD B&W formatter

--- src/test/UnitTest_svutResultFormatterStdBW.cpp
+++ src/test/UnitTest_svutResultFormatterStdBW.cpp
@@ -14,13 +14,93 @@
 #include <cppunit/extensions/HelperMacros.h>
 #include <svutResultFormatterStdBW.h>
 #include "UnitTestMockTestCase.h"
+#include <sys/stat.h>
 
 using namespace std;
 using namespace svUnitTest;
 using namespace CPPUNIT_NS;
 
 /********************  CONSTS  ***********************/
-static const char * SUMMARY_0 = "\n\
+static const char * CST_STRING_SUMMARY_0 = "\n\
++---------------------------+\n\
+|    SUMMARY                |\n\
++---------------------------+\n\
+|  SUCCESS  :    0 (  0 %)  |\n\
+|  INDEV    :    0 (  0 %)  |\n\
+|  TODO     :    0 (  0 %)  |\n\
+|  FAILED   :    0 (  0 %)  |\n\
+|  UNKNOWN  :    0 (  0 %)  |\n\
++---------------------------+\n\
+|  TOTAL    :    0          |\n\
+|  STATUS   : SUCCESS       |\n\
++---------------------------+\n";
+static const char * CST_STRING_UNKNOWN_1 =
+" * testMethod                                   [ UNKNOWN ]\n\
+- - - - - - - - - - - - - - - - - - - - - - - - - - - - - -\n\
+\n\
+unknown location\n\
+- - - - - - - - - - - - - - - - - - - - - - - - - - - - - -\n";
+static const char * CST_STRING_UNKNOWN_3 = " * testMethod                                   [ UNKNOWN ]\n\
+- - - - - - - - - - - - - - - - - - - - - - - - - - - - - -\n\
+message for unknown status\n\
+line 33 of file file.cpp on methode methode()\n\
+- - - - - - - - - - - - - - - - - - - - - - - - - - - - - -\n";
+static const char * CST_STRING_UNKNOWN_2 = " * testMethod                                   [ UNKNOWN ]\n";
+static const char * CST_STRING_SUCCESS_1 = " * testMethod                                   [ SUCCESS ]\n";
+static const char * CST_STRING_SUCCESS_2 = " * MyTest:testMethod                            [ SUCCESS ]\n";
+static const char * CST_STRING_TODO      = " * testMethod                                   [ TODO ]\n";
+static const char * CST_STRING_INDEV     = " * testMethod                                   [ INDEV ]\n";
+static const char * CST_STRING_SKIPED    = " * testMethod                                   [ SKIPED ]\n";
+static const char * CST_STRING_TEST_CASE = "=======  MyTest                                   ========\n";
+static const char * CST_STRING_FAILED_2  = " * testMethod                                   [ FAILED ]\n";
+static const char * CST_STRING_FAILED_1  = " * testMethod                                   [ FAILED ]\n\
+- - - - - - - - - - - - - - - - - - - - - - - - - - - - - -\n\
+message for failed status\n\
+line 33 of file file.cpp on methode methode()\n\
+   - actual : duck\n\
+   - expected : toto\n\
+- - - - - - - - - - - - - - - - - - - - - - - - - - - - - -\n";
+static const char * CST_STRING_SEQ_1    = "=======  MyTest                                   ========\n\
+ * testMethod                                   [ TODO ]\n\
+ * testMethod                                   [ INDEV ]\n\
+=======  MyTest                                   ========\n\
+ * testMethod                                   [ SKIPED ]\n\
+ * testMethod                                   [ FAILED ]\n\
+- - - - - - - - - - - - - - - - - - - - - - - - - - - - - -\n\
+message for unknown status\n\
+line 33 of file file.cpp on methode methode()\n\
+   - actual : duck\n\
+   - expected : toto\n\
+- - - - - - - - - - - - - - - - - - - - - - - - - - - - - -\n\
+=======  MyTest                                   ========\n\
+ * testMethod                                   [ UNKNOWN ]\n\
+- - - - - - - - - - - - - - - - - - - - - - - - - - - - - -\n\
+message for unknown status\n\
+line 33 of file file.cpp on methode methode()\n\
+- - - - - - - - - - - - - - - - - - - - - - - - - - - - - -\n\n\
++---------------------------+\n\
+|    SUMMARY                |\n\
++---------------------------+\n\
+|  SUCCESS  :    0 (  0 %)  |\n\
+|  INDEV    :    0 (  0 %)  |\n\
+|  TODO     :    0 (  0 %)  |\n\
+|  FAILED   :    0 (  0 %)  |\n\
+|  UNKNOWN  :    0 (  0 %)  |\n\
++---------------------------+\n\
+|  TOTAL    :    0          |\n\
+|  STATUS   : SUCCESS       |\n\
++---------------------------+\n";
+static const char * CST_STRING_SEQ_2    = "=======  MyTest                                   ========\n\
+ * testMethod                                   [ SUCCESS ]\n\
+ * testMethod                                   [ TODO ]\n\
+ * testMethod                                   [ SUCCESS ]\n\
+ * testMethod                                   [ INDEV ]\n\
+=======  MyTest                                   ========\n\
+ * testMethod                                   [ SKIPED ]\n\
+ * testMethod                                   [ SUCCESS ]\n\
+ * testMethod                                   [ FAILED ]\n\
+=======  MyTest                                   ========\n\
+ * testMethod                                   [ UNKNOWN ]\n\n\
 +---------------------------+\n\
 |    SUMMARY                |\n\
 +---------------------------+\n\
@@ -50,6 +130,10 @@ class UnitTest_svutResultFormatterStdBW : public TestCase
 	CPPUNIT_TEST(testCloseTestMethod_5);
 	CPPUNIT_TEST(testCloseTestMethod_6);
 	CPPUNIT_TEST(testCloseTestMethod_7);
+	CPPUNIT_TEST(testCloseTestMethod_8);
+	CPPUNIT_TEST(testCloseTestMethod_9);
+	CPPUNIT_TEST(testCloseTestMethod_10);
+	CPPUNIT_TEST(testCloseTestMethod_11);
 	CPPUNIT_TEST(testPrintSummary);
 	CPPUNIT_TEST(testGlobal_1);
 	CPPUNIT_TEST(testGlobal_2);
@@ -71,10 +155,15 @@ class UnitTest_svutResultFormatterStdBW : public TestCase
 		void testCloseTestMethod_5(void);
 		void testCloseTestMethod_6(void);
 		void testCloseTestMethod_7(void);
+		void testCloseTestMethod_8(void);
+		void testCloseTestMethod_9(void);
+		void testCloseTestMethod_10(void);
+		void testCloseTestMethod_11(void);
 		void testPrintSummary(void);
 		void testGlobal_1(void);
 		void testGlobal_2(void);
 	protected:
+		void runTotalSequence(void);
 		svutResultFormatterStdBW * formatter;
 		stringstream * out;
 };
@@ -111,55 +200,183 @@ void UnitTest_svutResultFormatterStdBW::testCloseTestCase(void )
 /********************  METHODE  *********************/
 void UnitTest_svutResultFormatterStdBW::testCloseTestMethod_1(void )
 {
-	CPPUNIT_FAIL("todo");
+	UnitTestMockTestCase testCase;
+	svutTestMethod meth("testMethod",NULL,SVUT_NO_LOCATION);
+	formatter->closeTestMethod(testCase,meth,SVUT_STATUS_SUCCESS);
+	CPPUNIT_ASSERT_EQUAL("",out->str());
 }
 
 /********************  METHODE  *********************/
 void UnitTest_svutResultFormatterStdBW::testCloseTestMethod_2(void )
 {
-	CPPUNIT_FAIL("todo");
+	UnitTestMockTestCase testCase;
+	svutTestMethod meth("testMethod",NULL,SVUT_NO_LOCATION);
+	formatter->setDisplaySuccess(true);
+	formatter->closeTestMethod(testCase,meth,SVUT_STATUS_SUCCESS);
+	CPPUNIT_ASSERT_EQUAL(CST_STRING_SUCCESS_1,out->str());
 }
 
 /********************  METHODE  *********************/
 void UnitTest_svutResultFormatterStdBW::testCloseTestMethod_3(void )
 {
-	CPPUNIT_FAIL("todo");
+	UnitTestMockTestCase testCase;
+	svutTestMethod meth("testMethod",NULL,SVUT_NO_LOCATION);
+	formatter->setDisplaySuccess(true);
+	formatter->setDisplayFullName(true);
+	formatter->closeTestMethod(testCase,meth,SVUT_STATUS_SUCCESS);
+	CPPUNIT_ASSERT_EQUAL(CST_STRING_SUCCESS_2,out->str());
 }
 
 /********************  METHODE  *********************/
 void UnitTest_svutResultFormatterStdBW::testCloseTestMethod_4(void )
 {
-	CPPUNIT_FAIL("todo");
+	UnitTestMockTestCase testCase;
+	svutTestMethod meth("testMethod",NULL,SVUT_NO_LOCATION);
+	formatter->closeTestMethod(testCase,meth,SVUT_STATUS_TODO);
+	CPPUNIT_ASSERT_EQUAL(CST_STRING_TODO,out->str());
 }
 
 /********************  METHODE  *********************/
 void UnitTest_svutResultFormatterStdBW::testCloseTestMethod_5(void )
 {
-	CPPUNIT_FAIL("todo");
+	UnitTestMockTestCase testCase;
+	svutTestMethod meth("testMethod",NULL,SVUT_NO_LOCATION);
+	formatter->closeTestMethod(testCase,meth,SVUT_STATUS_INDEV);
+	CPPUNIT_ASSERT_EQUAL(CST_STRING_INDEV,out->str());
 }
 
 /********************  METHODE  *********************/
 void UnitTest_svutResultFormatterStdBW::testCloseTestMethod_6(void )
 {
-	CPPUNIT_FAIL("todo");
+	UnitTestMockTestCase testCase;
+	svutTestMethod meth("testMethod",NULL,SVUT_NO_LOCATION);
+	formatter->closeTestMethod(testCase,meth,SVUT_STATUS_SKIPED);
+	CPPUNIT_ASSERT_EQUAL(CST_STRING_SKIPED,out->str());
 }
 
 /********************  METHODE  *********************/
 void UnitTest_svutResultFormatterStdBW::testCloseTestMethod_7(void )
 {
-	CPPUNIT_FAIL("todo");
+	UnitTestMockTestCase testCase;
+	svutTestMethod meth("testMethod",NULL,SVUT_NO_LOCATION);
+	formatter->closeTestMethod(testCase,meth,SVUT_STATUS_UNKNOWN);
+	CPPUNIT_ASSERT_EQUAL(CST_STRING_UNKNOWN_1,out->str());
 }
+
+/********************  METHODE  *********************/
+void UnitTest_svutResultFormatterStdBW::testCloseTestMethod_8(void )
+{
+	UnitTestMockTestCase testCase;
+	svutTestMethod meth("testMethod",NULL,SVUT_NO_LOCATION);
+	formatter->setDisplayDetails(false);
+	formatter->closeTestMethod(testCase,meth,SVUT_STATUS_UNKNOWN);
+	CPPUNIT_ASSERT_EQUAL(CST_STRING_UNKNOWN_2,out->str());
+}
+
+/********************  METHODE  *********************/
+void UnitTest_svutResultFormatterStdBW::testCloseTestMethod_9(void )
+{
+	UnitTestMockTestCase testCase;
+	svutTestMethod meth("testMethod",NULL,SVUT_NO_LOCATION);
+	svutCodeLocation location("file.cpp","methode",33);
+	svutStatusInfo info(SVUT_STATUS_UNKNOWN,"message for unknown status",location);
+	formatter->closeTestMethod(testCase,meth,info);
+	CPPUNIT_ASSERT_EQUAL(CST_STRING_UNKNOWN_3,out->str());
+}
+
+/********************  METHODE  *********************/
+void UnitTest_svutResultFormatterStdBW::testCloseTestMethod_10(void )
+{
+	UnitTestMockTestCase testCase;
+	svutTestMethod meth("testMethod",NULL,SVUT_NO_LOCATION);
+	svutCodeLocation location("file.cpp","methode",33);
+	svutStatusInfo info(SVUT_STATUS_FAILED,"message for failed status",location);
+	info.addEntry("expected","toto");
+	info.addEntry("actual","duck");
+	formatter->closeTestMethod(testCase,meth,info);
+	CPPUNIT_ASSERT_EQUAL(CST_STRING_FAILED_1,out->str());
+}
+
+/********************  METHODE  *********************/
+void UnitTest_svutResultFormatterStdBW::testCloseTestMethod_11(void )
+{
+	UnitTestMockTestCase testCase;
+	svutTestMethod meth("testMethod",NULL,SVUT_NO_LOCATION);
+	svutCodeLocation location("file.cpp","methode",33);
+	svutStatusInfo info(SVUT_STATUS_FAILED,"message for failed status",location);
+	info.addEntry("expected","toto");
+	info.addEntry("actual","duck");
+	formatter->setDisplayDetails(false);
+	formatter->closeTestMethod(testCase,meth,info);
+	CPPUNIT_ASSERT_EQUAL(CST_STRING_FAILED_2,out->str());
+}
+
+/********************  METHODE  *********************/
+void UnitTest_svutResultFormatterStdBW::runTotalSequence(void )
+{
+	UnitTestMockTestCase testCase;
+	svutCodeLocation location("file.cpp","methode",33);
+	svutTestMethod meth("testMethod",NULL,location);
+	svutStatusInfo infoFailed(SVUT_STATUS_FAILED,"message for unknown status",location);
+	infoFailed.addEntry("expected","toto");
+	infoFailed.addEntry("actual","duck");
+	svutStatusInfo infoUnknown(SVUT_STATUS_UNKNOWN,"message for unknown status",location);
+	svutResultSummary summary;
+
+	formatter->openOutput();
+
+	formatter->openTestCase(testCase);
+
+	formatter->openTestMethod(testCase,meth);
+	formatter->closeTestMethod(testCase,meth,SVUT_STATUS_SUCCESS);
+
+	formatter->openTestMethod(testCase,meth);
+	formatter->closeTestMethod(testCase,meth,SVUT_STATUS_TODO);
+
+	formatter->openTestMethod(testCase,meth);
+	formatter->closeTestMethod(testCase,meth,SVUT_STATUS_SUCCESS);
+
+	formatter->openTestMethod(testCase,meth);
+	formatter->closeTestMethod(testCase,meth,SVUT_STATUS_INDEV);
+
+	formatter->closeTestCase(testCase);
+
+	formatter->openTestCase(testCase);
+
+	formatter->openTestMethod(testCase,meth);
+	formatter->closeTestMethod(testCase,meth,SVUT_STATUS_SKIPED);
+
+	formatter->openTestMethod(testCase,meth);
+	formatter->closeTestMethod(testCase,meth,SVUT_STATUS_SUCCESS);
+
+	formatter->openTestMethod(testCase,meth);
+	formatter->closeTestMethod(testCase,meth,infoFailed);
+
+	formatter->openTestCase(testCase);
+	formatter->closeTestMethod(testCase,meth,infoUnknown);
+
+	formatter->closeTestCase(testCase);
+	
+	formatter->printSummary(summary);
+	
+	formatter->closeOutput();
+}
+
 
 /********************  METHODE  *********************/
 void UnitTest_svutResultFormatterStdBW::testGlobal_1(void )
 {
-	CPPUNIT_FAIL("todo");
+	this->runTotalSequence();
+	CPPUNIT_ASSERT_EQUAL(CST_STRING_SEQ_1,out->str());
 }
 
 /********************  METHODE  *********************/
 void UnitTest_svutResultFormatterStdBW::testGlobal_2(void )
 {
-	CPPUNIT_FAIL("todo");
+	formatter->setDisplaySuccess(true);
+	formatter->setDisplayDetails(false);
+	this->runTotalSequence();
+	CPPUNIT_ASSERT_EQUAL(CST_STRING_SEQ_2,out->str());
 }
 
 /********************  METHODE  *********************/
@@ -174,7 +391,7 @@ void UnitTest_svutResultFormatterStdBW::testOpenTestCase(void )
 {
 	UnitTestMockTestCase testCase;
 	formatter->openTestCase(testCase);
-	CPPUNIT_ASSERT_EQUAL("=======  MyTest                                   ========\n",out->str());
+	CPPUNIT_ASSERT_EQUAL(CST_STRING_TEST_CASE,out->str());
 }
 
 /********************  METHODE  *********************/
@@ -191,7 +408,7 @@ void UnitTest_svutResultFormatterStdBW::testPrintSummary(void )
 {
 	svutResultSummary summary;
 	formatter->printSummary(summary);
-	CPPUNIT_ASSERT_EQUAL(SUMMARY_0,out->str());
+	CPPUNIT_ASSERT_EQUAL(CST_STRING_SUMMARY_0,out->str());
 }
 
 CPPUNIT_TEST_SUITE_REGISTRATION(UnitTest_svutResultFormatterStdBW);
