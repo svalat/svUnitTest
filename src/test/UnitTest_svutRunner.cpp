6f99cd810c0cf2490ba06308305cf5f31c580cae
Valat Sébastien <sebastien.valat.dev@orange.fr>
2011-01-12 20:44:08 +0100

    * Working on #72  * Fix the last unit test  * Passing all tests

--- src/test/UnitTest_svutRunner.cpp
+++ src/test/UnitTest_svutRunner.cpp
@@ -27,7 +27,7 @@ static const char * CST_STRING_OUTPUT_0 = "=======  UnitTestMockTestCase2       
  * testFailure                                  [ FAILED ]\n\
 - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -\n\
 Failed on expected value.\n\
-line 51 of file " TEST_SRC_BASE_PATH "/UnitTestMockTestCase2.cpp on methode testFailure()\n\
+line 52 of file " TEST_SRC_BASE_PATH "/UnitTestMockTestCase2.cpp on methode testFailure()\n\
    - Actual : 3\n\
    - Expected : 2\n\
 - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -\n\
@@ -36,12 +36,12 @@ line 51 of file " TEST_SRC_BASE_PATH "/UnitTestMockTestCase2.cpp on methode test
  * testUnknown_1                                [ UNKNOWN ]\n\
 - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -\n\
 Unexpected std exception : std::bad_exception\n\
-line 133 of file " TEST_SRC_BASE_PATH "/UnitTestMockTestCase2.cpp on methode useTests()\n\
+line 134 of file " TEST_SRC_BASE_PATH "/UnitTestMockTestCase2.cpp on methode useTests()\n\
 - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -\n\
  * testUnknown_2                                [ UNKNOWN ]\n\
 - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -\n\
 Unexpected exception.\n\
-line 134 of file " TEST_SRC_BASE_PATH "/UnitTestMockTestCase2.cpp on methode useTests()\n\
+line 135 of file " TEST_SRC_BASE_PATH "/UnitTestMockTestCase2.cpp on methode useTests()\n\
 - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -\n\
  * testFailIsTodo_1                             [ TODO ]\n\
  * testFailIsTodo_2                             [ TODO ]\n\
@@ -115,7 +115,18 @@ static const char * CST_STRING_OUTPUT_7 = "testSuccess()\n\
 testTodo()\n\
 testIndev()\n";
 static const char * CST_STRING_OUTPUT_8 = "openOutput(); openTestCase(UnitTestMockTestCase2); openTestMethod(UnitTestMockTestCase2, testSuccess); closeTestMethod(UnitTestMockTestCase2, testSuccess, SUCCESS); closeTestCase(UnitTestMockTestCase2); success=1, failed=0, todo=0, indev=0, unknown=0, skiped=0; closeOutput(); ";
+static const char * CST_STRING_OUTPUT_9 = "openOutput(); openTestCase(MockTestUnload); closeTestCase(MockTestUnload); success=0, failed=0, todo=0, indev=0, unknown=0, skiped=0; closeOutput(); ";
 
+/*********************  CLASS  **********************/
+static bool UnitTest_svutRunner_MockStatus = false;
+class UnitTestMockTestUnload : public svutTestCase
+{
+	public:
+		UnitTestMockTestUnload();
+		virtual ~UnitTestMockTestUnload();
+		virtual void setUp(void ){};
+		virtual void tearDown(void ){};
+};
 
 /*********************  CLASS  **********************/
 class UnitTest_svutRunner : public TestCase
@@ -325,13 +336,39 @@ void UnitTest_svutRunner::testLoadAutoDetected(void)
 	CPPUNIT_ASSERT_EQUAL(true,localRunner.run());
 	ss << *formatter;
 	SVUT_ASSERT_EQUAL(CST_STRING_OUTPUT_5,ss.str());
+	localRunner.unloadAutoDetected();
 	clearTestCaseRegister();
+}
+
+/*******************  FUNCTION  *********************/
+UnitTestMockTestUnload::UnitTestMockTestUnload()
+	:svutTestCase("MockTestUnload")
+{
+	UnitTest_svutRunner_MockStatus = true;
+}
+
+/*******************  FUNCTION  *********************/
+UnitTestMockTestUnload::~UnitTestMockTestUnload()
+{
+	UnitTest_svutRunner_MockStatus = false;
 }
 
 /*******************  FUNCTION  *********************/
 void UnitTest_svutRunner::testUnloadAutoDetected(void)
 {
-	CPPUNIT_FAIL("todo");
+	stringstream ss;
+	svutRunner localRunner(*formatter);
+	this->mock->useTests(UnitTestMockTestCase2::REGISTER_SUCCESS);
+	svutTestCaseBuilderGeneric<UnitTestMockTestUnload> builder;
+	registerTestCase(builder);
+	localRunner.loadAutoDetected();
+	CPPUNIT_ASSERT_EQUAL(true,UnitTest_svutRunner_MockStatus);
+	CPPUNIT_ASSERT_EQUAL(true,localRunner.run());
+	ss << *formatter;
+	SVUT_ASSERT_EQUAL(CST_STRING_OUTPUT_9,ss.str());
+	localRunner.unloadAutoDetected();
+	CPPUNIT_ASSERT_EQUAL(false,UnitTest_svutRunner_MockStatus);
+	clearTestCaseRegister();
 }
 
 /*******************  FUNCTION  *********************/
