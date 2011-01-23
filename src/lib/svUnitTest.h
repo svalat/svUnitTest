94b4870287b74513c299f3c73ccce797772d5e43
Valat Sébastien <sebastien.valat.dev@orange.fr>
2011-01-23 19:41:48 +0100

    * Fix ticket #83 by adding svUnitTest.h header

---
+++ src/lib/svUnitTest.h
@@ -0,0 +1,21 @@
+/*****************************************************
+             PROJECT  : svUnitTest
+             VERSION  : 0.1.0
+             DATE     : 01/2011
+             AUTHOR   : Valat Sébastien
+             LICENSE  : CeCILL-C
+*****************************************************/
+
+#ifndef SVUNITTEST_HEADER
+#define SVUNITTEST_HEADER
+
+/********************  HEADERS  *********************/
+#include "svutTestCase.h"
+#include "svutAssert.h"
+#include "svutDefaultMain.h"
+
+/********************  MACRO  ***********************/
+/** Define the library version. **/
+#define SVUT_LIBARY_VERSION "0.1.0"
+
+#endif 
